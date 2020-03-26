3c30ee38bb20cbfb2784a117e070f28421b7af45
卢汝东 <lurudonga@outlook.com>
2020-03-26 20:57:05 +0800

    提交

---
+++ Destiny.Core.Flow/Destiny.Core.Flow/Exceptions/AppException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Destiny.Core.Flow.Exceptions
+{
+    public class AppException : Exception
+    {
+
+
+
+        public AppException()
+        {
+
+        }
+        public AppException(string message) : base(message)
+        {
+
+        }
+        public AppException(string message, Exception innerException) : base(message, innerException)
+        {
+
+        }
+    }
+}
