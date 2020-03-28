76032b177b923bca60cb1e99dff59b2fe9995bcf
wangzewei <1585955375@qq.com>
2020-03-28 13:03:36 +0800

    添加SeriLog

--- Destiny.Core.Flow/Destiny.Core.Flow.API/Startup.cs
+++ Destiny.Core.Flow/Destiny.Core.Flow.API/Startup.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
-using Destiny.Core.Flow.API.StartupExtension;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
