3979e448ecd8bdb7dcbf626a27eb0e3b12e34a29
wangzewei <1585955375@qq.com>
2020-03-28 13:08:42 +0800

    init

--- Destiny.Core.Flow/Destiny.Core.Flow.API/Startup.cs
+++ Destiny.Core.Flow/Destiny.Core.Flow.API/Startup.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Destiny.Core.Flow.API.StartupExtension;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
