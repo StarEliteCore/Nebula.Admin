0f15edc1fede52e1436f2444475eb986d93d9696
wangzewei <1585955375@qq.com>
2020-03-29 13:44:18 +0800

    添加迁移到数据库在指定程序集生成迁移文件

---
+++ Destiny.Core.Flow/Destiny.Core.Flow.API/Startups/EntityFrameworkCoreMySqlModule.cs
@@ -0,0 +1,27 @@
+﻿using Destiny.Core.Flow.DbContexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Destiny.Core.Flow.Extensions;
+
+namespace Destiny.Core.Flow.API.Startups
+{
+    public class EntityFrameworkCoreMySqlModule : EntityFrameworkCoreModuleBase
+    {
+        protected override IServiceCollection UseSql(IServiceCollection services)
+        {
+            var Assembly = typeof(EntityFrameworkCoreMySqlModule).GetTypeInfo().Assembly.GetName().Name;//获取程序集
+            var mysqlconn = services.GetConfiguration()["Destiny:DbContext:MysqlConnectionString"];
+            services.AddDbContext<DestinyContext>(oprions => {
+                oprions.UseMySql(mysqlconn, assembly => { assembly.MigrationsAssembly("Destiny.Core.Flow.Model"); });
+            });
+            return services;
+        }
+    }
+}
---
+++ Destiny.Core.Flow/Destiny.Core.Flow.Model/Migrations/20200329054146_createdb2.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Destiny.Core.Flow.Model.Migrations
+{
+    public partial class createdb2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Role",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(maxLength: 10, nullable: false),
+                    NormalizedName = table.Column<string>(maxLength: 50, nullable: true),
+                    ConcurrencyStamp = table.Column<string>(maxLength: 256, nullable: true),
+                    IsAdmin = table.Column<bool>(nullable: false),
+                    LastModifierUserId = table.Column<Guid>(nullable: true),
+                    LastModifierTime = table.Column<DateTime>(nullable: true),
+                    IsDeleted = table.Column<bool>(nullable: false, defaultValue: false),
+                    CreatorUserId = table.Column<Guid>(nullable: true),
+                    CreatedTime = table.Column<DateTime>(nullable: false),
+                    Description = table.Column<string>(maxLength: 500, nullable: true),
+                    Code = table.Column<string>(maxLength: 20, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Role", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RoleClaim",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    RoleId = table.Column<Guid>(nullable: false),
+                    ClaimType = table.Column<string>(maxLength: 500, nullable: true),
+                    ClaimValue = table.Column<string>(maxLength: 500, nullable: true),
+                    LastModifierUserId = table.Column<Guid>(nullable: true),
+                    LastModifierTime = table.Column<DateTime>(nullable: true),
+                    IsDeleted = table.Column<bool>(nullable: false, defaultValue: false),
+                    CreatorUserId = table.Column<Guid>(nullable: true),
+                    CreatedTime = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RoleClaim", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    UserName = table.Column<string>(maxLength: 20, nullable: false),
+                    NormalizedUserName = table.Column<string>(maxLength: 50, nullable: true),
+                    NickName = table.Column<string>(maxLength: 50, nullable: false),
+                    Email = table.Column<string>(nullable: true),
+                    NormalizeEmail = table.Column<string>(nullable: true),
+                    EmailConfirmed = table.Column<bool>(nullable: false),
+                    PasswordHash = table.Column<string>(nullable: true),
+                    HeadImg = table.Column<string>(nullable: true),
+                    SecurityStamp = table.Column<string>(nullable: true),
+                    ConcurrencyStamp = table.Column<string>(nullable: true),
+                    PhoneNumber = table.Column<string>(nullable: true),
+                    PhoneNumberConfirmed = table.Column<bool>(nullable: false),
+                    TwoFactorEnabled = table.Column<bool>(nullable: false),
+                    LockoutEnd = table.Column<DateTimeOffset>(nullable: true),
+                    LockoutEnabled = table.Column<bool>(nullable: false, defaultValue: false),
+                    AccessFailedCount = table.Column<int>(nullable: false, defaultValue: 0),
+                    IsSystem = table.Column<bool>(nullable: false),
+                    Sex = table.Column<int>(nullable: false),
+                    LastModifierUserId = table.Column<Guid>(nullable: true),
+                    LastModifierTime = table.Column<DateTime>(nullable: true),
+                    IsDeleted = table.Column<bool>(nullable: false, defaultValue: false),
+                    CreatorUserId = table.Column<Guid>(nullable: true),
+                    CreatedTime = table.Column<DateTime>(nullable: false),
+                    Description = table.Column<string>(maxLength: 500, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserClaim",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    UserId = table.Column<Guid>(nullable: false),
+                    ClaimType = table.Column<string>(nullable: false),
+                    ClaimValue = table.Column<string>(nullable: true),
+                    LastModifierUserId = table.Column<Guid>(nullable: true),
+                    LastModifierTime = table.Column<DateTime>(nullable: true),
+                    IsDeleted = table.Column<bool>(nullable: false, defaultValue: false),
+                    CreatorUserId = table.Column<Guid>(nullable: true),
+                    CreatedTime = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserClaim", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserRole",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    UserId = table.Column<Guid>(nullable: false),
+                    RoleId = table.Column<Guid>(nullable: false),
+                    LastModifierUserId = table.Column<Guid>(nullable: true),
+                    LastModifierTime = table.Column<DateTime>(nullable: true),
+                    IsDeleted = table.Column<bool>(nullable: false, defaultValue: false),
+                    CreatorUserId = table.Column<Guid>(nullable: true),
+                    CreatedTime = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserRole", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserToken",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    UserId = table.Column<Guid>(nullable: false),
+                    LoginProvider = table.Column<string>(maxLength: 450, nullable: true),
+                    Name = table.Column<string>(maxLength: 450, nullable: true),
+                    Value = table.Column<string>(nullable: true),
+                    LastModifierUserId = table.Column<Guid>(nullable: true),
+                    LastModifierTime = table.Column<DateTime>(nullable: true),
+                    IsDeleted = table.Column<bool>(nullable: false, defaultValue: false),
+                    CreatorUserId = table.Column<Guid>(nullable: true),
+                    CreatedTime = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserToken", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "User",
+                columns: new[] { "Id", "ConcurrencyStamp", "CreatedTime", "CreatorUserId", "Description", "Email", "EmailConfirmed", "HeadImg", "IsSystem", "LastModifierTime", "LastModifierUserId", "LockoutEnabled", "LockoutEnd", "NickName", "NormalizeEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "Sex", "TwoFactorEnabled", "UserName" },
+                values: new object[] { new Guid("a1e89f45-4fa8-4751-9df9-dec86f7e6c14"), "0286cab6-8a4a-44ed-9a97-86b0506c65c3", new DateTime(2020, 3, 29, 13, 41, 46, 685, DateTimeKind.Local).AddTicks(9989), null, "系统管理员拥有所有权限", null, false, null, true, null, null, true, null, "管理员", null, "ADMIN", "AQAAAAEAACcQAAAAEEPWhHPCHU1i6Z0ayoApKGbPlZUb38RUdJg4QjUcccVhUSto0sRZtLOXfwWUJ+P2Xw==", null, false, "3OWMGQAK5ZTXMSV6OFSGIWWWNIWJ2SX6", 0, false, "Admin" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Role");
+
+            migrationBuilder.DropTable(
+                name: "RoleClaim");
+
+            migrationBuilder.DropTable(
+                name: "User");
+
+            migrationBuilder.DropTable(
+                name: "UserClaim");
+
+            migrationBuilder.DropTable(
+                name: "UserRole");
+
+            migrationBuilder.DropTable(
+                name: "UserToken");
+        }
+    }
+}
