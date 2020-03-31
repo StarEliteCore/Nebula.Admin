1f87c22d913b03fa15e3e0b39dbe6c1b6894c173
1585955375@qq.com <1585955375@qq.com>
2020-03-31 22:44:53 +0800

    【add】数据字典添加完成

---
+++ src/Destiny.Core.Flow.API/Controllers/DataDictionary/DataDictionaryController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Destiny.Core.Flow.AspNetCore.Ui;
+using Destiny.Core.Flow.Dtos.DataDictionnary;
+using Destiny.Core.Flow.IServices.IDataDictionnary;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Destiny.Core.Flow.API.Controllers.DataDictionary
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataDictionaryController : ControllerBase
+    {
+        private readonly IDataDictionnaryServices _dataDictionnaryServices  = null;
+
+        public DataDictionaryController(IDataDictionnaryServices dataDictionnaryServices)
+        {
+            _dataDictionnaryServices = dataDictionnaryServices;
+        }
+
+        // GET: api/DataDictionary
+        [HttpPost]
+        public async Task<AjaxResult> Post([FromBody]DataDictionnaryInputDto dto)
+        {
+            return (await _dataDictionnaryServices.CreateAsync(dto)).ToAjaxResult();
+        }
+    }
+}
