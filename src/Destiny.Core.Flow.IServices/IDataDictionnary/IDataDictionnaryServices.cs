a7569078041498d507f2be31a4f1ee05f5c11464
1585955375@qq.com <1585955375@qq.com>
2020-03-31 23:04:40 +0800

    忘记勾选代码未提交

---
+++ src/Destiny.Core.Flow.IServices/IDataDictionnary/IDataDictionnaryServices.cs
@@ -0,0 +1,18 @@
+﻿using Destiny.Core.Flow.Dtos.DataDictionnary;
+using Destiny.Core.Flow.Ui;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Destiny.Core.Flow.IServices.IDataDictionnary
+{
+    public interface IDataDictionnaryServices
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        Task<OperationResponse> CreateAsync(DataDictionnaryInputDto input);
+    }
+}
---
+++ src/Destiny.Core.Flow.Services/DataDictionnary/DataDictionnaryServices.cs
@@ -0,0 +1,35 @@
+﻿using Destiny.Core.Flow.Dependency;
+using Destiny.Core.Flow.Dtos.DataDictionnary;
+using Destiny.Core.Flow.Extensions;
+using Destiny.Core.Flow.IServices.IDataDictionnary;
+using Destiny.Core.Flow.Model.Entities.Dictionary;
+using Destiny.Core.Flow.Repository.DictionaryRepository;
+using Destiny.Core.Flow.Ui;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Destiny.Core.Flow.Services.DataDictionnary
+{
+    [Dependency(ServiceLifetime.Scoped)]
+    public class DataDictionnaryServices: IDataDictionnaryServices
+    {
+        private readonly IDataDictionnaryRepository _dataDictionnaryRepository = null;
+        public DataDictionnaryServices(IDataDictionnaryRepository dataDictionnaryRepository)
+        {
+            _dataDictionnaryRepository = dataDictionnaryRepository;
+        }
+        public async Task<OperationResponse> CreateAsync(DataDictionnaryInputDto input)
+        {
+            input.NotNull(nameof(input));
+            var dictionnary = input.MapTo<DataDictionary>();
+            var operationResponse = new OperationResponse("添加成功", Enums.OperationResponseType.Success);
+            operationResponse.Success = await _dataDictionnaryRepository.InsertAsync(dictionnary) > 0;
+            operationResponse.Message = operationResponse.Success ? "添加成功" : "添加失败";
+            return operationResponse;
+        }
+
+    }
+}
