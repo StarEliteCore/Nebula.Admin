fb1df300a35abec7fd51a686c66d430976ce6582
1585955375@qq.com <1585955375@qq.com>
2020-05-18 22:03:13 +0800

    【add】接入动态lambda表达式查询

---
+++ src/Destiny.Core.Flow/Filter/QueryFilter.cs
@@ -0,0 +1,26 @@
+﻿using Destiny.Core.Flow.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Destiny.Core.Flow.Filter
+{
+    public class QueryFilter
+    {
+        public QueryFilter()
+        {
+
+        }
+        public QueryFilter(FilterConnect filterConnect, List<FilterCondition> filters)
+        {
+            this.FilterConnect = filterConnect;
+            this.Filters = filters;
+        }
+        /// <summary>
+        /// 查询条件and或者Or
+        /// </summary>
+        public FilterConnect FilterConnect { get; set; } = FilterConnect.And;
+
+        public List<FilterCondition> Filters { get; set; } = new List<FilterCondition>();
+    }
+}
