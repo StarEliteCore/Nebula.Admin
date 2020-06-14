a54d8f8f447d61934962445598c48034c4e3f6b6
dong <179722134@qq.com>
2020-06-14 12:13:47 +0800

    有疑问加上注释

--- src/Destiny.Core.Flow.Model/Entities/Rolemenu/RolemenuEntity.cs
+++ src/Destiny.Core.Flow.Model/Entities/Rolemenu/RolemenuEntity.cs
@@ -7,8 +7,8 @@ using System.Text;
 
 namespace Destiny.Core.Flow.Model.Entities.Rolemenu
 {
-    [DisplayName("角色菜单实体")]
-    public class RolemenuEntity : EntityBase<Guid>
+    [DisplayName("角色菜单")] //为什么要实体？？？？？？？后缀Entity，全部用软删除
+    public class RoleMenuEntity : EntityBase<Guid>
     {
         /// <summary>
         /// 角色ID
