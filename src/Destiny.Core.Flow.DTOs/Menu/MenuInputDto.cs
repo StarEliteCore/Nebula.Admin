b9476f7a7d38e79f2b43a3f5fc7de1988c4fe536
1585955375@qq.com <1585955375@qq.com>
2020-06-17 23:35:46 +0800

    【update】修改菜单inputDto

--- src/Destiny.Core.Flow.DTOs/Menu/MenuInputDto.cs
+++ src/Destiny.Core.Flow.DTOs/Menu/MenuInputDto.cs
@@ -25,19 +25,23 @@ namespace Destiny.Core.Flow.Dtos.Menu
         /// 组件地址(前端)
         /// </summary>
         [DisplayName("组件地址(前端)")]
-        public string RouterPath { get; set; }
+        public string Path { get; set; }
 
         /// <summary>
         /// 父级菜单ID
         /// </summary>
         [DisplayName("父级菜单ID")]
         public Guid ParentId { get; set; }
-
+        /// <summary>
+        /// 组件地址
+        /// </summary>
+        [DisplayName("组件地址")]
+        public string Component { get; set; }
         /// <summary>
         /// 菜单图标
         /// </summary>
         [DisplayName("菜单图标")]
-        public string Iocn { get; set; }
+        public string Icon { get; set; }
         /// <summary>
         ///获取或设置 描述
         /// </summary>
