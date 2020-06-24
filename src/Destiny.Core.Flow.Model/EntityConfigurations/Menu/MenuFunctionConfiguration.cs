ae18a546aa5c6e37888f37cb30123b08a97f3d84
1585955375@qq.com <1585955375@qq.com>
2020-06-24 21:44:48 +0800

    【add】添加菜单功能数据库表

---
+++ src/Destiny.Core.Flow.Model/EntityConfigurations/Menu/MenuFunctionConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Destiny.Core.Flow.Model.Entities.Menu;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Destiny.Core.Flow.Model.EntityConfigurations.Menu
+{
+    public class MenuFunctionConfiguration : EntityMappingConfiguration<MenuFunction, Guid>
+    {
+        public override void Map(EntityTypeBuilder<MenuFunction> b)
+        {
+            b.HasKey(o => o.Id);
+            b.Property(o => o.MenuId);
+            b.Property(o => o.FunctionId);
+            b.ToTable("MenuFunction");
+        }
+    }
+}
