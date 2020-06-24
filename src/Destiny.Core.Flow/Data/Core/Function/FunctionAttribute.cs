5e0bc408a9c064ad62e22125a6711fd2ab587d38
dong <179722134@qq.com>
2020-06-24 22:46:15 +0800

    添加特性

---
+++ src/Destiny.Core.Flow/Data/Core/Function/FunctionAttribute.cs
@@ -0,0 +1,13 @@
+﻿using Destiny.Core.Flow.Attributes.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Destiny.Core.Flow.Data.Core
+{
+    [AttributeUsage(AttributeTargets.Class|AttributeTargets.Method)]
+    public class FunctionAttribute : Attribute
+    {
+      
+    }
+}
