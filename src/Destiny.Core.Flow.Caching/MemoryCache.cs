46fb8f39d527ce6c5f248867ae5454d94c691f21
dong <179722134@qq.com>
2020-07-25 21:42:00 +0800

    修改缓存

---
+++ src/Destiny.Core.Flow.Caching/MemoryCache.cs
@@ -0,0 +1,102 @@
+﻿using Destiny.Core.Flow.Extensions;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Destiny.Core.Flow.Caching
+{
+    public class MemoryCache : ICache
+    {
+
+        private  IMemoryCache _cache=null;
+
+        public MemoryCache(IMemoryCache cache)
+        {
+            _cache = cache;
+        }
+
+        public TCacheData Get<TCacheData>(string key)
+        {
+            return (TCacheData)_cache.Get(key);
+        }
+
+        public async Task<TCacheData> GetAsync<TCacheData>(string key, CancellationToken token = default)
+        {
+
+            return await Task.Run(()=> (TCacheData)_cache.Get(key));
+        }
+
+        public TCacheData GetOrAdd<TCacheData>(string key, Func<TCacheData> func)
+        {
+            key.NotNull(nameof(key));
+
+            func.NotNull(nameof(func));
+            var value = this.Get<TCacheData>(key);
+
+
+            if (!Equals(value, default(TCacheData)))
+            {
+                return value;
+            }
+
+            value = func();
+
+            if (Equals(value, default(TCacheData)))
+            {
+                return default;
+            }
+
+            Set(key, value);
+            return value;
+        }
+
+        public async Task<TCacheData> GetOrAddAsync<TCacheData>([NotNull] string key, Func<Task<TCacheData>> func, CancellationToken token = default)
+        {
+            key.NotNull(nameof(key));
+
+            func.NotNull(nameof(func));
+            var value =await this.GetAsync<TCacheData>(key);
+
+
+            if (!Equals(value, default(TCacheData)))
+            {
+                return value;
+            }
+
+            value = await func();
+
+            if (Equals(value, default(TCacheData)))
+            {
+                return default;
+            }
+
+             await SetAsync(key, value);
+            return value;
+        }
+
+        public void Remove(string key)
+        {
+            _cache.Remove(key);
+        }
+
+        public async Task RemoveAsync(string key, CancellationToken token = default)
+        {
+            await Task.Run(() => this.Remove(key),token);
+        }
+
+        public void Set<TCacheData>(string key, TCacheData value)
+        {
+            _cache.Set(key,value);
+        }
+
+        public async Task SetAsync<TCacheData>(string key, TCacheData value, CancellationToken token = default)
+        {
+            await Task.Run(()=>this.Set<TCacheData>(key,value),token);
+        }
+    }
+}
