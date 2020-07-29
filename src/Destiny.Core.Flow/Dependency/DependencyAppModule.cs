b01ec792ffc98ebbe2a28e2b04962315a78fe2b7
dong <179722134@qq.com>
2020-07-29 21:42:22 +0800

    修改注入模块

--- src/Destiny.Core.Flow/Dependency/DependencyAppModule.cs
+++ src/Destiny.Core.Flow/Dependency/DependencyAppModule.cs
@@ -4,6 +4,7 @@ using Destiny.Core.Flow.Reflection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging.Abstractions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -29,9 +30,11 @@ namespace Destiny.Core.Flow.Dependency
         /// <param name="services"></param>
         private void BulkIntoServices(IServiceCollection services)
         {
+
             var typeFinder = services.GetOrAddSingletonService<ITypeFinder, TypeFinder>();
             typeFinder.NotNull(nameof(typeFinder));
-            Type[] dependencyTypes = typeFinder.Find(type => type.IsClass && !type.IsAbstract && !type.IsInterface && type.HasAttribute<DependencyAttribute>());
+            Type[] baseTypes = new[] { typeof(ISingletonDependency), typeof(IScopedDependency), typeof(ITransientDependency) };
+            Type[] dependencyTypes = typeFinder.Find(type => type.IsClass && !type.IsAbstract && !type.IsInterface && (type.HasAttribute<DependencyAttribute>()|| (baseTypes.Any(b => b.IsAssignableFrom(type)))));
             foreach (var dependencyType in dependencyTypes)
             {
                 AddToServices(services, dependencyType);
@@ -45,27 +48,66 @@ namespace Destiny.Core.Flow.Dependency
         /// <param name="implementationType">要注册的服务实例类型</param>
         protected virtual void AddToServices(IServiceCollection services, Type implementationType)
         {
-
-            var atrr = implementationType.GetAttribute<DependencyAttribute>();
-            Type[] serviceTypes = implementationType.GetImplementedInterfaces().Where(o=>!o.HasAttribute<IgnoreDependencyAttribute>()).ToArray();
+            if (implementationType.IsAbstract || implementationType.IsInterface)
+            {
+                return;
+            }
+            var lifetime=  GetServiceLifetime(implementationType);
+            if (lifetime == null)
+            {
+                return;
+            }
+              var atrr = implementationType.GetAttribute<DependencyAttribute>();
+         
+             Type[] serviceTypes = implementationType.GetImplementedInterfaces().Where(t=>!t.HasAttribute<IgnoreDependencyAttribute>()).ToArray();
 
             if (serviceTypes.Length == 0)
             {
-                services.TryAdd(new ServiceDescriptor(implementationType, implementationType, atrr.Lifetime));
+                services.TryAdd(new ServiceDescriptor(implementationType, implementationType, lifetime.Value));
                 return;
             }
 
             if (atrr?.AddSelf == true)
             {
-                services.TryAdd(new ServiceDescriptor(implementationType, implementationType, atrr.Lifetime));
+                services.TryAdd(new ServiceDescriptor(implementationType, implementationType, lifetime.Value));
             }
 
-            foreach (var interfaceType in serviceTypes)
+
+            for (int i = 0; i < serviceTypes.Length; i++)
             {
-                services.Add(new ServiceDescriptor(interfaceType, implementationType, atrr.Lifetime));
+                Type serviceType = serviceTypes[i];
+                ServiceDescriptor descriptor = new ServiceDescriptor(serviceType, implementationType, lifetime.Value);
+
+                services.Add(descriptor);
             }
         }
 
+
+        private ServiceLifetime? GetServiceLifetime(Type type)
+        {
+            var dependency = type.GetAttribute<DependencyAttribute>();
+            if (dependency != null)
+            {
+                return dependency.Lifetime;
+            }
+
+            if (type.IsDeriveClassFrom<ISingletonDependency>())
+            {
+               return  ServiceLifetime.Singleton;
+            }
+
+            if (type.IsDeriveClassFrom<ITransientDependency>())
+            {
+                return ServiceLifetime.Transient;
+            }
+
+            if (type.IsDeriveClassFrom<IScopedDependency>())
+            {
+                return ServiceLifetime.Scoped;
+            }
+            return null;
+
+        }
         public override void Configure(IApplicationBuilder applicationBuilder)
         {
             IocManage.Instance.SetApplicationServiceProvider(applicationBuilder.ApplicationServices);
