6367be7f46ff5ee57eb8971442442c7915261622
1585955375@qq.com <1585955375@qq.com>
2020-08-08 16:48:26 +0800

    【Add】添加日志审计事件

--- src/Destiny.Core.Flow.EntityFrameworkCore/DbContexts/DefaultDbContext.cs
+++ src/Destiny.Core.Flow.EntityFrameworkCore/DbContexts/DefaultDbContext.cs
@@ -1,17 +1,40 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Destiny.Core.Flow.Audit;
+using Destiny.Core.Flow.Events.EventBus;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Destiny.Core.Flow
 {
     public class DefaultDbContext : DbContextBase
     {
+        private readonly IServiceProvider _serviceProvider;
+        private readonly IEventBus _bus;
         public DefaultDbContext(DbContextOptions<DefaultDbContext> options, IServiceProvider serviceProvider)
           : base(options, serviceProvider)
         {
-
+            _serviceProvider = serviceProvider;
+            _bus = _serviceProvider.GetService<IEventBus>();
         }
-
+        protected override async Task AfterSaveChanges()
+        {
+            await _bus.PublishAsync(new AuditEvent() { Entries = this.ChangeTracker.Entries() });
+        }
+        public override int SaveChanges()
+        {
+            return base.SaveChanges();
+        }
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            var result = await base.SaveChangesAsync(cancellationToken);
+            await this.AfterSaveChanges();//
+            return result;
+        }
     }
 }
