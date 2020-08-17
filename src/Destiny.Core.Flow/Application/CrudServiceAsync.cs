bec523eac4a0bb545d5d454590194e764e8377a1
dong <179722134@qq.com>
2020-08-17 21:51:18 +0800

    添加查读修添加实现

--- src/Destiny.Core.Flow/Application/CrudServiceAsync.cs
+++ src/Destiny.Core.Flow/Application/CrudServiceAsync.cs
@@ -9,42 +9,5 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Destiny.Core.Flow.Application
 {
-    //public class CrudServiceAsync<TEntity, TPrimaryKey> : ICrudServiceAsync<TEntity, TPrimaryKey>
-    //        where TEntity : IEntity<TPrimaryKey>, IEquatable<TPrimaryKey>
-    //{
-    //    private readonly IServiceProvider _serviceProvider = null;
-    //    private readonly IEFCoreRepository<TEntity, TPrimaryKey> _efCoreRepository = null;
-
-    //    public CrudServiceAsync(IServiceProvider serviceProvider)
-    //    {
-    //        _serviceProvider = serviceProvider;
-    //        _efCoreRepository = _serviceProvider.GetService<IEFCoreRepository<TEntity, TPrimaryKey>>();
-    //    }
-
-    //    public Task<OperationResponse> DeleteAsync(TPrimaryKey primaryKey)
-    //    {
-           
-    //    }
-    //    protected virtual Task<OperationResponse> BeforeDeleteAsync(TEntity entity)
-    //    {
-    //        OperationResponse response = new OperationResponse();
-       
-    //        return Task.FromResult(response);
-    //    }
-
-    //    protected virtual Task<OperationResponse> AfterDeleteAsync(TEntity entity)
-    //    {
-    //        return Task.FromResult(Ok());
-    //    }
-
-    //    public IQueryable<TEntity> Query()
-    //    {
-    //      return  _efCoreRepository.Entities;
-    //    }
-
-    //    public IQueryable<TEntity> QueryNoTracking()
-    //    {
-    //        return _efCoreRepository.TrackEntities;
-    //    }
-    //}
+  
 }
