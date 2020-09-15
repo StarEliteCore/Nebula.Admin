9fd596ae92dff19adad12ffcd33f793301446372
LiGuo <liguo1976@outlook.com>
2020-09-15 09:50:07 +0800

    修复构建错误

---
+++ src/Destiny.Core.Flow.Dtos/Identitys/ChangePassInputDto.cs
@@ -0,0 +1,25 @@
+﻿namespace Destiny.Core.Flow.Dtos
+{
+    /// <summary>
+    /// 修改密码
+    /// </summary>
+    public class ChangePassInputDto
+    {
+        /// <summary>
+        /// 用户名
+        /// </summary>
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// 旧密码
+        /// </summary>
+        public string OldPassword { get; set; }
+
+        /// <summary>
+        /// 新密码
+        /// </summary>
+
+        public string NewPassword { get; set; }
+
+    }
+}
