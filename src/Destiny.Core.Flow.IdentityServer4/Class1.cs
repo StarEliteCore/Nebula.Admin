cdc6f9c595d7f24aa57499f2bcd9d822fab1aacf
1585955375 <1585955375@qq.com>
2020-09-16 13:43:57 +0800

    [Add]添加IdentityServer4模块，准备介入Ids4

---
+++ src/Destiny.Core.Flow.IdentityServer4/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Destiny.Core.Flow.IdentityServer4
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ src/Destiny.Core.Flow.Model/Entities/IdentityServer4/IdsApiSecret.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using IdentityServer4.EntityFramework.Entities;
+
+namespace Destiny.Core.Flow.Model.Entities.IdentityServer4
+{
+    //public class IdsApiSecret: ApiSecret
+    //{
+    //}
+}
