c366d9ddf84ada9f4e0fb4b3e743cc69ab7c7a75
1585955375 <1585955375@qq.com>
2020-09-21 13:27:24 +0800

    【ADD】添加IdentityServer4API资源数据持久化重写

---
+++ src/Destiny.Core.Flow.IdentityServer4/IdentityServerProfile/ApiResourceMapperProfile.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using Destiny.Core.Flow.Model.DestinyIdentityServer4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Destiny.Core.Flow.IdentityServer.IdentityServerProfile
+{
+    public class ApiResourceMapperProfile: Profile
+    {
+        public ApiResourceMapperProfile()
+        {
+            // entity to model
+            CreateMap<ApiResource, IdentityServer4.Models.ApiResource>(MemberList.Destination)
+                .ForMember(x => x.Properties,
+                    opt => opt.MapFrom(src => src.Properties.ToDictionary(item => item.Key, item => item.Value)))
+                .ForMember(x => x.ApiSecrets, opt => opt.MapFrom(src => src.Secrets.Select(x => x)))
+                .ForMember(x => x.Scopes, opt => opt.MapFrom(src => src.Scopes.Select(x => x)))
+                .ForMember(x => x.UserClaims, opts => opts.MapFrom(src => src.UserClaims.Select(x => x.Type)));
+            CreateMap<ApiResourceSecret, IdentityServer4.Models.Secret>(MemberList.Destination);
+            CreateMap<ApiScope, IdentityServer4.Models.ApiScope>(MemberList.Destination)
+                .ForMember(x => x.UserClaims, opt => opt.MapFrom(src => src.UserClaims.Select(x => x.Type)));
+
+            // model to entity
+            CreateMap<IdentityServer4.Models.ApiResource, ApiResource>(MemberList.Source)
+                .ForMember(x => x.Properties,
+                    opt => opt.MapFrom(src => src.Properties.ToDictionary(item => item.Key, item => item.Value)))
+                .ForMember(x => x.Secrets, opts => opts.MapFrom(src => src.ApiSecrets.Select(x => x)))
+                .ForMember(x => x.Scopes, opts => opts.MapFrom(src => src.Scopes.Select(x => x)))
+                .ForMember(x => x.UserClaims, opts => opts.MapFrom(src => src.UserClaims.Select(x => new ApiResourceClaim { Type = x })));
+            CreateMap<IdentityServer4.Models.Secret, ApiResourceSecret>(MemberList.Source);
+            CreateMap<IdentityServer4.Models.ApiScope, ApiScope>(MemberList.Source)
+                .ForMember(x => x.UserClaims, opts => opts.MapFrom(src => src.UserClaims.Select(x => new ApiScopeClaim { Type = x })));
+        }
+    }
+}
---
+++ src/Destiny.Core.Flow.IdentityServer4/Store/ResourceStoreBase.cs
@@ -0,0 +1,50 @@
+﻿using Destiny.Core.Flow.Model.DestinyIdentityServer4;
+using IdentityServer4.Stores;
+using System;
+using System.Collections.Generic;
+using System.Linq.Dynamic.Core;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using Destiny.Core.Flow.Extensions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Destiny.Core.Flow.IdentityServer.Store
+{
+    public class ResourceStoreBase : IResourceStore
+    {
+        private readonly IEFCoreRepository<ApiResource, Guid> _apiResourceRepository;
+
+        public ResourceStoreBase(IEFCoreRepository<ApiResource, Guid> apiResourceRepository)
+        {
+            _apiResourceRepository = apiResourceRepository;
+        }
+
+        public async Task<IEnumerable<IdentityServer4.Models.ApiResource>> FindApiResourcesByNameAsync(IEnumerable<string> apiResourceNames)
+        {
+            var model= await _apiResourceRepository.Entities.Where(x => apiResourceNames.Contains(x.Name)).ToListAsync();
+            var dto=model.Select(x => x.MapTo<IdentityServer4.Models.ApiResource>()).ToList();
+            return dto;
+        }
+
+        public async Task<IEnumerable<IdentityServer4.Models.ApiResource>> FindApiResourcesByScopeNameAsync(IEnumerable<string> scopeNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<IdentityServer4.Models.ApiScope>> FindApiScopesByNameAsync(IEnumerable<string> scopeNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<IdentityServer4.Models.IdentityResource>> FindIdentityResourcesByScopeNameAsync(IEnumerable<string> scopeNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IdentityServer4.Models.Resources> GetAllResourcesAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
