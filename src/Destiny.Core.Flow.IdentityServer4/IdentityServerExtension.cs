f3b420c4cefdfaa06ffc779dce0ba83c27fbd9b9
LiGuo <liguo1976@outlook.com>
2020-09-21 18:05:24 +0800

    添加使用自定义存储扩展方法

---
+++ src/Destiny.Core.Flow.IdentityServer4/IdentityServerExtension.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using IdentityServer4.Stores;
+using Destiny.Core.Flow.IdentityServer.Store;
+
+namespace Destiny.Core.Flow.IdentityServer
+{
+    public static class IdentityServerExtension
+    {
+        public static IIdentityServerBuilder AddDestinyIdentityServer(this IIdentityServerBuilder builder)
+        {
+            //builder.Services.AddTransient<IPersistedGrantStore, PersistedGrantStore>();
+            //builder.Services.AddTransient<IDeviceFlowStore, DeviceFlowStore>();
+            return builder.AddClientStore<ClientStoreBase>()
+                .AddResourceStore<ApiResourceStoreBase>()
+                .AddPersistedGrantStore<PersistedGrantStoreBase>();
+        }
+    }
+}
