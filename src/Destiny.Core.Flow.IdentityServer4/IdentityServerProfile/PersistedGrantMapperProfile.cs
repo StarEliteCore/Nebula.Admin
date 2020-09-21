3c26c68dc348279209c27ea8aacc5d62c2268033
1585955375 <1585955375@qq.com>
2020-09-21 16:54:08 +0800

    【add】添加IPersistedGrantStore重写代码

---
+++ src/Destiny.Core.Flow.IdentityServer4/IdentityServerProfile/PersistedGrantMapperProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Destiny.Core.Flow.Model.DestinyIdentityServer4;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Destiny.Core.Flow.IdentityServer.IdentityServerProfile
+{
+    public class PersistedGrantMapperProfile: Profile
+    {
+        public PersistedGrantMapperProfile()
+        {
+            // entity to model
+            CreateMap<PersistedGrant, IdentityServer4.Models.PersistedGrant>(MemberList.Destination);
+
+            // model to entity
+            CreateMap<IdentityServer4.Models.PersistedGrant, PersistedGrant>(MemberList.Source);
+        }
+    }
+}
