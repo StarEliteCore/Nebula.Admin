098d07e4213e88d8488897cc0c78d62844a76dc7
1585955375 <1585955375@qq.com>
2020-09-22 13:24:03 +0800

    【Add】添加IdentityServer4模块配置类

---
+++ src/Destiny.Core.Flow.IdentityServer4/IdentityServer4Module.cs
@@ -0,0 +1,35 @@
+﻿using Destiny.Core.Flow.IdentityServer.Store;
+using Destiny.Core.Flow.Modules;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Destiny.Core.Flow.IdentityServer
+{
+    public class IdentityServer4Module : AppModule
+    {
+        public override void ConfigureServices(ConfigureServicesContext context)
+        {
+            var service = context.Services;
+            var build= service.AddIdentityServer(opt =>
+            {
+                opt.Events.RaiseErrorEvents = true;
+                opt.Events.RaiseInformationEvents = true;
+                opt.Events.RaiseFailureEvents = true;
+                opt.Events.RaiseSuccessEvents = true;
+            }).AddClientStore<ClientStoreBase>()
+            .AddResourceStore<ApiResourceStoreBase>()
+            .AddPersistedGrantStore<PersistedGrantStoreBase>();
+            build.AddDeveloperSigningCredential();
+        }
+        public override void ApplicationInitialization(ApplicationContext context)
+        {
+            var app = context.GetApplicationBuilder();
+            app.UseIdentityServer();
+        }
+
+        
+    }
+}
