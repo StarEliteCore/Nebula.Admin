e80bd8f5010cfe181c3289c56669eca1d8f78823
1585955375@qq.com <1585955375@qq.com>
2020-09-23 23:29:26 +0800

    【Update】调试售前客户端数据，但是还有问题待解决

---
+++ src/Destiny.Core.Flow.IdentityServer4/SeedDatas/IdentityServer4ApiResourceSeedData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Destiny.Core.Flow.IdentityServer
+{
+    public class IdentityServer4ApiResourceSeedData
+    {
+
+    }
+}
--- src/Destiny.Core.Flow.IdentityServer4/Store/ClientStoreBase.cs
+++ src/Destiny.Core.Flow.IdentityServer4/Store/ClientStoreBase.cs
@@ -14,19 +14,25 @@ namespace Destiny.Core.Flow.IdentityServer.Store
         private readonly ILogger<ClientStoreBase> _logger;
 
         private readonly IEFCoreRepository<Client, Guid> _clientRepository;
+        private readonly IEFCoreRepository<ClientGrantType, Guid> _clientGrantTypeRepository;
 
-        public ClientStoreBase(ILogger<ClientStoreBase> logger, IEFCoreRepository<Client, Guid> clientRepository)
+        public ClientStoreBase(ILogger<ClientStoreBase> logger, IEFCoreRepository<Client, Guid> clientRepository, IEFCoreRepository<ClientGrantType, Guid> clientGrantTypeRepository)
         {
             _logger = logger;
             _clientRepository = clientRepository;
+            _clientGrantTypeRepository = clientGrantTypeRepository;
         }
 
         public async Task<IdentityServer4.Models.Client> FindClientByIdAsync(string clientId)
         {
             var client = await _clientRepository
-                .Entities.Where(x => x.ClientId == clientId)
-                .FirstOrDefaultAsync();
-            return client?.MapTo<IdentityServer4.Models.Client>();
+                .Entities.Where(x => x.ClientId == clientId).FirstOrDefaultAsync();
+            if(client==null)
+                return client?.MapTo<IdentityServer4.Models.Client>();
+            var dto= client?.MapTo<IdentityServer4.Models.Client>();
+            var grantyypes= await _clientGrantTypeRepository.Entities.Where(x => x.ClientId == client.Id).Select(x => x.GrantType).ToListAsync();
+            dto.AllowedGrantTypes = grantyypes;
+            return dto;
         }
     }
 }
