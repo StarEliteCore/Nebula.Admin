f3f171d0d55caeedeca9ded60f76ce42ede6e508
1585955375 <1585955375@qq.com>
2020-09-23 17:17:03 +0800

    [Update]修复认证中心运行时报错

--- src/Destiny.Core.Flow.AuthenticationCenter/Startups/AppWebModule.cs
+++ src/Destiny.Core.Flow.AuthenticationCenter/Startups/AppWebModule.cs
@@ -1,19 +1,24 @@
-﻿using Destiny.Core.Flow.Extensions;
+﻿using Destiny.Core.Flow.Dependency;
+using Destiny.Core.Flow.Events;
+using Destiny.Core.Flow.Extensions;
 using Destiny.Core.Flow.IdentityServer;
 using Destiny.Core.Flow.Modules;
 using Destiny.Core.Flow.Options;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Threading.Tasks;
 
 namespace Destiny.Core.Flow.AuthenticationCenter.Startups
 {
     [DependsOn(
     typeof(IdentityModule),
-    //typeof(DependencyAppModule),
+    typeof(DependencyAppModule),
+    typeof(EventBusAppModule),
     typeof(EntityFrameworkCoreMySqlModule),
         typeof(IdentityServer4Module)
     )]
@@ -29,6 +34,12 @@ namespace Destiny.Core.Flow.AuthenticationCenter.Startups
         {
             var service = context.Services;
             service.AddMvc();
+            context.Services.AddTransient<IPrincipal>(provider =>
+            {
+                IHttpContextAccessor accessor = provider.GetService<IHttpContextAccessor>();
+                return accessor?.HttpContext?.User;
+            });
+
             var configuration = service.GetConfiguration();
             service.Configure<AppOptionSettings>(configuration.GetSection("Destiny"));
             var settings = service.GetAppSettings();
