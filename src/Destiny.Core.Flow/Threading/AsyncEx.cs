de30228550f9d7d4cc2c1d20dad259dad92e685b
dong <179722134@qq.com>
2020-10-06 20:18:09 +0800

    添加异步扩展

---
+++ src/Destiny.Core.Flow/Threading/AsyncEx.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Destiny.Core.Flow.Extensions
+{
+    /// <summary>
+    /// Task扩展
+    /// </summary>
+    public static partial class Extensions
+    {
+
+        /// <summary>
+        /// 处理Task异常（这种设计可能违反了非局部性原则）
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="TError"></typeparam>
+        /// <param name="task"></param>
+        /// <param name="onError"></param>
+        /// <returns></returns>
+        public static Task<T> Catch<T, TError>(this Task<T> task, Func<TError, T> onError) where TError : Exception
+        {
+            var tcs = new TaskCompletionSource<T>(); //TaskCompletionSource的实例返回一个Task类型以保持异步模型中的一致性
+            task.ContinueWith(innerTask =>
+            {
+                if (innerTask.IsFaulted && innerTask?.Exception?.InnerException is TError)
+                {
+                    tcs.SetResult(onError((TError)innerTask.Exception.InnerException));
+                }
+                else if (innerTask.IsCanceled)
+                {
+                    tcs.SetCanceled();
+                }
+                else if (innerTask.IsFaulted)
+                {
+                    tcs.SetException(innerTask?.Exception?.InnerException ?? throw new InvalidOperationException());
+                }
+                else
+                {
+                    tcs.SetResult(innerTask.Result);
+                }
+            });
+            return tcs.Task;
+        }
+
+        /// <summary>
+        /// 异步循环
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="maxDegreeOfParallelism"></param>
+        /// <param name="body"></param>
+        /// <returns></returns>
+        public static Task ForEachAsync<T>(this IEnumerable<T> source, int maxDegreeOfParallelism, Func<T, Task> body)
+        {
+            return Task.WhenAll(
+                from partition in Partitioner.Create(source).GetPartitions(maxDegreeOfParallelism)
+                select Task.Run(async () =>
+                {
+                    using (partition)
+                    {
+                        while (partition.MoveNext())
+                            await body(partition.Current);
+                    }
+                }));
+        }
+
+        /// <summary>
+        /// 异步循环
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="body"></param>
+        /// <returns></returns>
+        public static Task ForEachAsync<T>(this IEnumerable<T> source, Func<T, Task> body)
+        {
+            return source.ForEachAsync(Environment.ProcessorCount, body);
+        }
+
+        public static Task<TOut> Select<TIn, TOut>(this Task<TIn> task, Func<TIn, TOut> projection)
+        {
+            var r = new TaskCompletionSource<TOut>();
+            task.ContinueWith(self =>
+            {
+                if (self.IsFaulted)
+                {
+                    r.SetException(self.Exception.InnerExceptions);
+                }
+                else if (self.IsCanceled)
+                {
+                    r.SetCanceled();
+                }
+                else
+                {
+                    r.SetResult(projection(self.Result));
+                }
+            });
+            return r.Task;
+        }
+
+        public static Task<TOut> SelectMany<TIn, TOut>(this Task<TIn> first, Func<TIn, Task<TOut>> next)
+        {
+            var tcs = new TaskCompletionSource<TOut>();
+            first.ContinueWith(delegate
+            {
+                if (first.IsFaulted)
+                {
+                    tcs.TrySetException(first.Exception.InnerExceptions);
+                }
+                else if (first.IsCanceled)
+                {
+                    tcs.TrySetCanceled();
+                }
+                else
+                {
+                    try
+                    {
+                        var t = next(first.Result);
+                        if (t == null) tcs.TrySetCanceled();
+                        else
+                            t.ContinueWith(delegate
+                            {
+                                if (t.IsFaulted)
+                                {
+                                    tcs.TrySetException(t.Exception.InnerExceptions);
+                                }
+                                else if (t.IsCanceled)
+                                {
+                                    tcs.TrySetCanceled();
+                                }
+                                else
+                                {
+                                    tcs.TrySetResult(t.Result);
+                                }
+                            }, TaskContinuationOptions.ExecuteSynchronously);
+                    }
+                    catch (Exception exc)
+                    {
+                        tcs.TrySetException(exc);
+                    }
+                }
+            }, TaskContinuationOptions.ExecuteSynchronously);
+            return tcs.Task;
+        }
+
+        /// <summary>
+        /// 完成时处理
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="inputTasks"></param>
+        /// <returns></returns>
+        public static IEnumerable<Task<T>> ProcessAsComplete<T>(this IEnumerable<Task<T>> inputTasks)
+        {
+            // Copy the input so we know it’ll be stable, and we don’t evaluate it twice
+            var inputTaskList = inputTasks.ToList();
+            // Could use Enumerable.Range here, if we wanted…
+            var completionSourceList = new List<TaskCompletionSource<T>>(inputTaskList.Count);
+            for (var i = 0; i < inputTaskList.Count; i++) completionSourceList.Add(new TaskCompletionSource<T>());
+
+            // At any one time, this is "the index of the box we’ve just filled".
+            // It would be nice to make it nextIndex and start with 0, but Interlocked.Increment
+            // returns the incremented value…
+            var prevIndex = -1;
+
+            // We don’t have to create this outside the loop, but it makes it clearer
+            // that the continuation is the same for all tasks.
+            Action<Task<T>> continuation = completedTask =>
+            {
+                var index = Interlocked.Increment(ref prevIndex);
+                var source = completionSourceList[index];
+                switch (completedTask.Status)
+                {
+                    case TaskStatus.Canceled:
+                        source.TrySetCanceled();
+                        break;
+                    case TaskStatus.Faulted:
+                        source.TrySetException(completedTask.Exception.InnerExceptions);
+                        break;
+                    case TaskStatus.RanToCompletion:
+                        source.TrySetResult(completedTask.Result);
+                        break;
+                    default:
+                        throw new ArgumentException("Task was not completed");
+                }
+            };
+
+            foreach (var inputTask in inputTaskList)
+                inputTask.ContinueWith(continuation,
+                    CancellationToken.None,
+                    TaskContinuationOptions.ExecuteSynchronously,
+                    TaskScheduler.Default);
+
+            return completionSourceList.Select(source => source.Task);
+        }
+    }
+}
---
+++ src/Destiny.Core.Flow/Threading/TaskResult.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Destiny.Core.Flow.Threading
+{
+  
+    public struct TaskResult<T>
+    {
+        public T Ok { get; } 
+        public Exception Error { get; } 
+
+        public bool IsFailed => Error != null;
+        public bool IsOk => !IsFailed;
+
+        public TaskResult(T ok)
+        {
+            Ok = ok;
+            Error = default;
+        }
+
+        public TaskResult(Exception error) 
+        {
+            Error = error;
+            Ok = default;
+        }
+
+        public R Match<R>(Func<T, R> okMap, Func<Exception, R> failureMap)
+        {
+            return IsOk ? okMap(Ok) : failureMap(Error); 
+        }
+
+        public void Match(Action<T> okAction, Action<Exception> errorAction)
+        {
+            if (IsOk) okAction(Ok);
+            else errorAction(Error); 
+        }
+
+        public static implicit operator TaskResult<T>(T ok)
+        {
+            return new TaskResult<T>(ok); 
+        }
+
+        public static implicit operator TaskResult<T>(Exception error)
+        {
+            return new TaskResult<T>(error);
+        }
+
+        public static implicit operator TaskResult<T>(TaskResult.Ok<T> ok)
+        {
+            return new TaskResult<T>(ok.Value); 
+        }
+
+        public static implicit operator TaskResult<T>(TaskResult.Failure error)
+        {
+            return new TaskResult<T>(error.Error); 
+        }
+    }
+
+    public static class TaskResult
+    {
+        public struct Ok<L>
+        {
+            internal L Value { get; }
+
+            internal Ok(L value)
+            {
+                Value = value;
+            }
+        }
+
+        public struct Failure
+        {
+            internal Exception Error { get; }
+
+            internal Failure(Exception error)
+            {
+                Error = error;
+            }
+        }
+    }
+
+  
+    public static class ResultExtensions
+    {
+        public static TaskResult.Ok<T> Ok<T>(T value)
+        {
+            return new TaskResult.Ok<T>(value);
+        }
+
+        public static TaskResult.Failure Failure(Exception error)
+        {
+            return new TaskResult.Failure(error);
+        }
+
+        public static async Task<TaskResult<T>> TryCatch<T>(Func<Task<T>> func)
+        {
+            try
+            {
+                return await func();
+            }
+            catch (Exception ex)
+            {
+                return ex;
+            }
+        }
+
+        public static Task<TaskResult<T>> TryCatch<T>(Func<T> func)
+        {
+            return TryCatch(() => Task.FromResult(func()));
+        }
+
+        public static async Task<TaskResult<R>> SelectMany<T, R>(this Task<TaskResult<T>> resultTask,
+            Func<T, Task<TaskResult<R>>> func)
+        {
+            var result = await resultTask.ConfigureAwait(false);
+            if (result.IsFailed)
+                return result.Error;
+            return await func(result.Ok);
+        }
+
+        public static async Task<TaskResult<R>> Select<T, R>(this Task<TaskResult<T>> resultTask, Func<T, Task<R>> func)
+        {
+            var result = await resultTask.ConfigureAwait(false);
+            if (result.IsFailed)
+                return result.Error;
+            return await func(result.Ok).ConfigureAwait(false);
+        }
+
+        public static async Task<TaskResult<R>> Match<T, R>(this Task<TaskResult<T>> resultTask, Func<T, Task<R>> actionOk,
+            Func<Exception, Task<R>> actionError)
+        {
+            var result = await resultTask.ConfigureAwait(false);
+            if (result.IsFailed)
+                return await actionError(result.Error);
+            return await actionOk(result.Ok);
+        }
+
+      
+
+        public static async Task<TaskResult<R>> OnSuccess<T, R>(this Task<TaskResult<T>> resultTask, Func<T, Task<R>> func)
+        {
+            var result = await resultTask.ConfigureAwait(false);
+
+            if (result.IsFailed)
+                return result.Error;
+            return await func(result.Ok).ConfigureAwait(false);
+        }
+
+        public static async Task<TaskResult<T>> OnFailure<T>(this Task<TaskResult<T>> resultTask, Func<Task> func)
+        {
+            var result = await resultTask.ConfigureAwait(false);
+
+            if (result.IsFailed)
+                await func().ConfigureAwait(false);
+            return result;
+        }
+
+        public static async Task<TaskResult<R>> Bind<T, R>(this Task<TaskResult<T>> resultTask, Func<T, Task<TaskResult<R>>> func)
+        {
+            var result = await resultTask.ConfigureAwait(false);
+
+            if (result.IsFailed)
+                return result.Error;
+            return await func(result.Ok).ConfigureAwait(false);
+        }
+
+        public static async Task<TaskResult<R>> Map<T, R>(this Task<TaskResult<T>> resultTask, Func<T, Task<R>> func)
+        {
+            var result = await resultTask.ConfigureAwait(false);
+
+            if (result.IsFailed)
+                return result.Error;
+
+            return await TryCatch(() => func(result.Ok));
+        }
+
+        public static async Task<TaskResult<R>> Match<T, R>(this Task<TaskResult<T>> resultTask, Func<T, R> actionOk,
+            Func<Exception, R> actionError)
+        {
+            var result = await resultTask.ConfigureAwait(false);
+
+            if (result.IsFailed)
+                return actionError(result.Error);
+            return actionOk(result.Ok);
+        }
+
+        public static async Task<TaskResult<T>> Ensure<T>(this Task<TaskResult<T>> resultTask, Func<T, Task<bool>> predicate,
+            string errorMessage)
+        {
+            var result = await resultTask.ConfigureAwait(false);
+
+            if (result.IsFailed || !await predicate(result.Ok).ConfigureAwait(false)) return result.Error;
+            return result.Ok;
+        }
+
+        public static async Task<TaskResult<T>> Tap<T>(this Task<TaskResult<T>> resultTask, Func<T, Task> action)
+        {
+            var result = await resultTask.ConfigureAwait(false);
+
+            if (result.IsOk)
+                await action(result.Ok).ConfigureAwait(false);
+            return result;
+        }
+    }
+}
