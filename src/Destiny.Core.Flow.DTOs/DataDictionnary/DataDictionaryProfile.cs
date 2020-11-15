a94e05f4f7acf8cac57ab68a222dc9815be39096
1162321341@qq.com <1162321341@qq.com>
2020-11-15 18:37:51 +0800

    数据字典

---
+++ src/Destiny.Core.Flow.DTOs/DataDictionnary/DataDictionaryProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Destiny.Core.Flow.Model.Entities.Dictionary;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Destiny.Core.Flow.Dtos.DataDictionnary
+{
+    public class DataDictionaryProfile : Profile
+    {
+        public DataDictionaryProfile()
+        {
+            CreateMap<DataDictionaryEntity, DataDictionaryOutDto>().ForMember(x => x.Title, opt => opt.MapFrom(x => x.Title)).ForMember(x => x.Key, opt => opt.MapFrom(x => x.Id));
+        }
+    }
+}
