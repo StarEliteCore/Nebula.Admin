86679b9a41979ed6177ac7676dd2768065d40f05
卢汝东 <179722134@qq.com>
2020-11-15 21:35:37 +0800

    1，添加菜单功能的时候 已经有了的菜单不用显示在选择器中 2，支持多选分配菜单功能

---
+++ src/Destiny.Core.Flow.Dtos/Functions/FunctionPageRequestDto.cs
@@ -0,0 +1,13 @@
+﻿using Destiny.Core.Flow.Filter;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Destiny.Core.Flow.Dtos.Functions
+{
+    public  class FunctionPageRequestDto: PageRequest
+    {
+
+       public Guid?[] MenuIds { get; set; }
+    }
+}
