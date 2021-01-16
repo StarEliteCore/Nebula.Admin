02142cfe8301903cec6dbdaa90e12b749ec0a198
卢汝东 <179722134@qq.com>
2021-01-16 12:32:32 +0800

    删除不必要文件，添加是否文件扩展

--- src/Destiny.Core.Flow/Extensions/FileExtensions.cs
+++ src/Destiny.Core.Flow/Extensions/FileExtensions.cs
@@ -1,6 +1,26 @@
-﻿namespace Destiny.Core.Flow.Extensions
+﻿using System.IO;
+
+namespace Destiny.Core.Flow.Extensions
 {
-    public class FileExtensions
+    public static class FileExtensions
     {
+
+        /// <summary>
+        /// 判断是否文件
+        /// </summary>
+        /// <param name="fileName">要判断文件名</param>
+        /// <param name="fileExtension">要判断文件后缀名</param>
+        /// <returns>如何是返回ture,则返回false</returns>
+        public static bool IsFile(this string fileName,string fileExtension)
+        {
+
+            if (Path.GetExtension(fileExtension).ToLower() != fileExtension) //txt文件
+            {
+
+                return false;
+            }
+            return true;
+
+        }
     }
 }
