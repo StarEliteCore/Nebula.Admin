84c8a4ffc8e413a000afedb2a8c90807324b6995
卢汝东 <179722134@qq.com>
2021-01-21 22:45:12 +0800

    1.添加Api资源控制器，接口，DTO

---
+++ src/Destiny.Core.Flow.IServices/IdentityServer4/IApiResourceService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Destiny.Core.Flow.Dependency;
+using Destiny.Core.Flow.Dtos.IdentityServer4;
+using Destiny.Core.Flow.Ui;
+
+namespace Destiny.Core.Flow.IServices.IdentityServer4
+{
+    /// <summary>
+    /// API资源服务
+    /// </summary>
+    public interface IApiResourceService : IScopedDependency
+    {
+        
+        /// <summary>
+        /// 异步创建Api资源
+        /// </summary>
+        /// <param name="dto">要伟入DTO</param>
+        /// <returns></returns>
+        Task<OperationResponse>  CreateApiResourceAsync(ApiResourceInputDto dto);
+    }
+}
