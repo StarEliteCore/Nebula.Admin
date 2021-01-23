934ee44642194e00a920c19c558803a3b39e1df9
1585955375@qq.com <1585955375@qq.com>
2021-01-23 20:11:31 +0800

    【Add】添加IdentitServer4的一些配置文件

---
+++ src/Destiny.Core.Flow/IdentityServer4/GrantType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Destiny.Core.Flow
+{
+    public static class GrantType
+    {
+        public const string Implicit = "implicit";
+
+        public const string Hybrid = "hybrid";
+
+        public const string AuthorizationCode = "authorization_code";
+
+        public const string ClientCredentials = "client_credentials";
+
+        public const string ResourceOwnerPassword = "password";
+
+        public const string DeviceFlow = "urn:ietf:params:oauth:grant-type:device_code";
+    }
+}
