051f3618a4018847a7f9bac43b3d81c6a74533e1
大黄瓜 <179722134@qq.com>
2021-04-06 22:38:31 +0800

    修改CrudServiceAsync

--- src/Destiny.Core.Flow/Application/ICrudServiceAsync.cs
+++ src/Destiny.Core.Flow/Application/ICrudServiceAsync.cs
@@ -8,11 +8,23 @@ namespace Destiny.Core.Flow.Application
     public interface ICrudServiceAsync<TEntity, TPrimaryKey>
             where TEntity : IEntity<TPrimaryKey>, IEquatable<TPrimaryKey>
     {
+
+
         /// <summary>
-        /// 删除
+        /// 异步添加
         /// </summary>
-        /// <param name="primaryKey"></param>
+        /// <param name="entity"></param>
         /// <returns></returns>
-        Task<OperationResponse> DeleteAsync(TPrimaryKey primaryKey);
+        Task<OperationResponse> AddAsync(TEntity entity);
+
+        /// <summary>
+        /// 异步更新
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        Task<OperationResponse> UpdateAsync(TEntity entity);
+
+
+        Task<OperationResponse> DeleteAsync(TPrimaryKey key);
     }
 }
