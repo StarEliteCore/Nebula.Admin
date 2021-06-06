d535a3fbdb615ea92b4d9cc6b0d4fd960adc042d
大黄瓜 <179722134@qq.com>
2021-06-06 22:01:00 +0800

    修改权限

--- src/Destiny.Core.Flow.API/Filters/PermissionAuthorizationFilter.cs
+++ src/Destiny.Core.Flow.API/Filters/PermissionAuthorizationFilter.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Controllers;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -23,21 +24,95 @@ namespace Destiny.Core.Flow.API.Filters
     /// </summary>
     public class PermissionAuthorizationFilter: IAsyncAuthorizationFilter
     {
-        private readonly IAuthorityVerification _authority;
+        private readonly IAuthorityVerification _authority = null;
         private readonly IPrincipal _principal;
         private readonly ILogger<PermissionAuthorizationFilter> _logger = null;
+        private readonly IServiceProvider _serviceProvider = null;
 
-        public PermissionAuthorizationFilter(IAuthorityVerification authority, IPrincipal principal, ILogger<PermissionAuthorizationFilter> logger)
+        public PermissionAuthorizationFilter(IServiceProvider serviceProvider)
         {
-            _authority = authority;
-            authority.NotNull(nameof(authority));
-            _principal = principal;
-            _logger = logger;
+            _serviceProvider = serviceProvider;
+            _principal = serviceProvider.GetService<IPrincipal>();
+            _logger = serviceProvider.GetService<ILoggerFactory>()?.CreateLogger<PermissionAuthorizationFilter>();
+            _authority = serviceProvider.GetService<IAuthorityVerification>();
+
         }
+
+
+        protected async Task OnCheckAuthorzieAsync(AuthorizationFilterContext context)
+        {
+
+            if (!context.HttpContext.User.Identity.IsAuthenticated) //没有通过登录
+            {
+                context.HttpContext.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                context.Result = new JsonResult(new AjaxResult(MessageDefinitionType.Unauthorized, AjaxResultType.Unauthorized));
+                return;
+
+            }
+            else {
+                var httpContext=  context.HttpContext;
+                if (!await this.CheckHandlerAsync(context, httpContext))
+                {
+                    context.HttpContext.Response.StatusCode = StatusCodes.Status403Forbidden;
+                    context.Result = new JsonResult(new AjaxResult(MessageDefinitionType.Uncertified, AjaxResultType.Uncertified));
+                    return;
+
+                }
+                
+            }
+        }
+
+        /// <summary>
+        /// 验证处理
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="httpContext"></param>
+        /// <returns></returns>
+        protected async Task<bool> CheckHandlerAsync(AuthorizationFilterContext context, HttpContext httpContext)
+        {
+
+            var authority=  this._serviceProvider.GetService<IAuthorityVerification>();
+            var action = context.ActionDescriptor as ControllerActionDescriptor;
+            var linkurl = context.HttpContext.Request.Path.Value.Replace("/api/", "");
+            var result = new AjaxResult(MessageDefinitionType.Unauthorized, AjaxResultType.Unauthorized);
+            if (!action.EndpointMetadata.Any(x => x is AllowAnonymousAttribute))
+            {
+                if (!_principal.Identity.IsAuthenticated)
+                {
+                    context.HttpContext.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                    context.Result = new JsonResult(result);
+                    return false;
+                }
+                else
+                {
+                    if (!action.EndpointMetadata.Any(x => x is NoAuthorityVerificationAttribute))
+                    {
+                        _logger.LogError($"此{linkurl}地址没有权限");
+                        var result1 = (await authority.IsPermission(linkurl.ToLower()));
+                        if (result1.Type != AuthResultType.Success)
+                        {
+
+                            return false;
+                        }
+                        else {
+
+                            context.HttpContext.Response.StatusCode = StatusCodes.Status200OK;
+                            context.Result = new JsonResult(new AjaxResult("登录成功", AjaxResultType.Success));
+                            return true;
+                        }
+                      
+                    }
+                }
+            }
+            return false;
+        }
+
+
 
         public async Task OnAuthorizationAsync(AuthorizationFilterContext context)
         {
             _logger.LogInformation($"进入权限判断");
+
             var action = context.ActionDescriptor as ControllerActionDescriptor;
             var linkurl = context.HttpContext.Request.Path.Value.Replace("/api/", "");
             var result = new AjaxResult(MessageDefinitionType.Unauthorized, AjaxResultType.Unauthorized);
